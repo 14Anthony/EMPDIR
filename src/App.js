ac547037d4328c3a4a374b5069dfe2aa3cee4e05
Anthony Belcastro <belcastt@live.com>
2020-10-16 06:31:29 -0700

    watch where you are in the files reload

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-
 import classes from './App.css';
 import Person from './Person/Person';
+
+
+
 
 class App extends Component {
   state = {
     persons: [
-      { id: 'asfa1', name: 'Max', age: 28 },
-      { id: 'vasdf1', name: 'Manu', age: 29 },
-      { id: 'asdf11', name: 'Stephanie', age: 26 }
+      { id: '1', name: 'Travis', tenure: 28, ext: 1234 },
+      { id: '2', name: 'Blake', tenure: 9, ext: 234 },
+      { id: '3', name: 'Joe', tenure: 2, ext: 12 },
+      { id: '4', name: 'Matt', tenure: 29, ext: 567 },
+      { id: '5', name: 'Daniel', tenure: 8, ext: 987 },
+      { id: '6', name: 'Frank', tenure: 15, ext: 237 },
+      { id: '7', name: 'Superman', tenure: 26, ext: 6543 }
     ],
     otherState: 'some other value',
     showPersons: false
-  };
+  }
 
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
@@ -31,64 +37,68 @@ class App extends Component {
     persons[personIndex] = person;
 
     this.setState({ persons: persons });
-  };
+  }
 
-  deletePersonHandler = personIndex => {
+  deletePersonHandler = (personIndex) => {
     // const persons = this.state.persons.slice();
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
     this.setState({ persons: persons });
-  };
+  }
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
     this.setState({ showPersons: !doesShow });
-  };
+  }
 
   render() {
+
+
     let persons = null;
-    let btnClass = '';
 
     if (this.state.showPersons) {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return (
-              <Person
-                click={() => this.deletePersonHandler(index)}
-                name={person.name}
-                age={person.age}
-                key={person.id}
-                changed={event => this.nameChangedHandler(event, person.id)}
-              />
-            );
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              tenure={person.tenure}
+              ext={person.ext}
+              id={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
         </div>
       );
+      // style.backgroundColor = 'orangered'
+      // style[':hover'] = {
+      //   backgroundColor: 'lightblue',
+      //   color: 'black'
+      //   }
+      // }
 
-      btnClass = classes.Red;
-    }
+      let assignedClasses = [];
 
-    const assignedClasses = [];
-    if (this.state.persons.length <= 2) {
-      assignedClasses.push(classes.red); // classes = ['red']
-    }
-    if (this.state.persons.length <= 1) {
-      assignedClasses.push(classes.bold); // classes = ['red', 'bold']
-    }
+      if (this.state.persons.length <= 3) {
+        assignedClasses.push(classes.bolder);
 
-    return (
-      <div className={classes.App}>
-        <h1>Hi, I'm a React App</h1>
-        <p className={assignedClasses.join(' ')}>This is really working!</p>
-        <button className={btnClass} onClick={this.togglePersonsHandler}>
-          Toggle Persons
-        </button>
-        {persons}
-      </div>
-    );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
-  }
-}
+      }
+      if (this.state.persons.length <= 1) {
+        assignedClasses.push(classes.orangered)
+      }
+      return (
 
-export default App;
+
+        <div className={classes.App}>
+          <h1>Employee Directory</h1>
+          <p className={assignedClasses.join('')}>Meet your Collegues</p>
+          <button
+            className={classes.Button}
+            onClick={this.togglePersonsHandler}> Toggle Employee</button>
+          {persons}
+        </div>
+
+      );
+
+
+      export default App;
