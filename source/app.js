6be216f7692ed27a427ddbcb9b9b123f2cc4c0c5
Anthony Belcastro <belcastt@live.com>
2020-10-16 03:00:18 -0700

    getting rid of the psudo

--- source/app.js
+++ source/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Person from './components/person';
+import './App.css';
 
 
 class App extends Component {
@@ -16,39 +17,24 @@ class App extends Component {
         otherState: 'Changed State',
         showPersons: false
     };
-    //  POSSIBLE RESUSEABLE CODE FROM TODO REPOconst filterTodos = (term) => {
 
-    //     Array.from(list.children)
-    //     .filter((todo) => !todo.textContent.toLowerCase().includes(term))
-    //     // console.log(todo.textContent);
-    //     // return true;
-
-    //     .forEach((todo) => todo.classList.add('filtered'));
-
-
-    // Array.from(list.children)
-    //     .filter((todo) => todo.textContent.toLowerCase().includes(term))
-    //     // console.log(todo.textContent);
-    //     // return true;
-
-    //     .forEach((todo) => todo.classList.remove('filtered'));
-
-    // };
-
-
-    // search.addEventListener('keyup', () => {
-
-    // const term = search.value.trim().toLowerCase();
-    // filterTodos(term);
-
-    // });
 
     render() {
+
+        const style = {
+            backgroundColor: 'white',
+            font: 'inherit',
+            border: '1px solid blue',
+            padding: '8px',
+            cursor: 'pointer'
+        };
+
         return (
             <div className="App">
                 <h1>Employee Director</h1>
                 <p></p>
                 <p></p>
+                <button>filter names</button>
                 <Person id={this.state.persons[0].id} name={this.state.persons[0].name} tenure={this.state.persons[0].tenure} ext={this.state.persons[0].ext} />
                 <Person id={this.state.persons[1].id} name={this.state.persons[1].name} tenure={this.state.persons[1].tenure} ext={this.state.persons[1].ext} />
                 <Person id={this.state.persons[2].id} name={this.state.persons[2].name} tenure={this.state.persons[2].tenure} ext={this.state.persons[2].ext} />
